563c86c9d1fab0ed5900f33bc2b5d709dca7ae82
Benjamin Staffin <benley@gmail.com>
2015-08-16 15:54:12 -0700

    Basic lslocks

---
+++ lslocks.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+"""Print PIDs and paths of files locked in a directory tree."""
+from __future__ import print_function
+
+import collections
+import logging as log
+import os
+import sys
+
+
+def gen_imap(rootdir):
+    """inode-to-filepath map for descendents of rootdir.
+
+    Args:
+        rootdir (String): directory path.
+
+    Returns:
+        {inode: {filepath, ...}}
+    """
+
+    imap = collections.defaultdict(set)
+
+    def _dopath(base, path):
+        fpath = os.path.join(base, path)
+        try:
+            imap[os.stat(fpath).st_ino].add(fpath)
+        except OSError as err:
+            log.warn('Skipping %s (%s)', fpath, err.strerror)
+
+    for (dirpath, dirnames, filenames) in os.walk(rootdir):
+        for dirname in dirnames:
+            _dopath(dirpath, dirname)
+        for filename in filenames:
+            _dopath(dirpath, filename)
+    return imap
+
+
+def read_locks(locks_fh):
+    """Process /proc/locks into (inode, pid) tuples.
+
+    Args:
+      locks_fh: file-like object
+
+    Yields:
+      (inode, pid)
+    """
+
+    try:
+        while True:
+            # locknum: type subtype read/write pid maj:min:inode rStart rEnd
+            (_, _, _, _, l_pid, l_fileid, _, _) = locks_fh.next().split()
+            _, _, l_inode = l_fileid.split(':')
+            yield (l_inode, l_pid)
+    except StopIteration:
+        return
+
+
+def filter_locks(locks, inode_map):
+    """Filter a set of locks to those matching inode_map.
+
+    Args:
+      locks: Iterable of (inode, pid) pairs.
+      inode_map: Mapping of {inode: [filepath, ...]}
+
+    Yields:
+      (pid, [filepath, ...])
+    """
+    for (inode, pid) in locks:
+        _inode = int(inode)
+        if _inode in inode_map:
+            yield (pid, inode_map[_inode])
+
+
+def main(rootpath):
+    """Main"""
+    inode_map = gen_imap(rootpath)
+
+    with open('/proc/locks') as locks_fh:
+        locks = filter_locks(read_locks(locks_fh), inode_map)
+
+        print('PID\tPath')
+        for (pid, locked_paths) in locks:
+            for path in locked_paths:
+                print('%s\t%s' % (pid, path))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.stderr.write('USAGE: %s <directory>\n')
+        sys.exit(1)
+    main(sys.argv[1])
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""setup.py"""
+
+import setuptools
+
+setuptools.setup(
+    name='lslocks',
+    author='Benjamin Staffin',
+    author_email='benley@gmail.com',
+    scripts=['lslocks.py']
+)
